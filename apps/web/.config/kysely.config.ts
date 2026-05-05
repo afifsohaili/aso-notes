b683a467ca407bbf0ac3d68768300878b357ad80
Afif Sohaili <afifnajib@gmail.com>
2026-05-06 00:00:02 +0800

    fix(kysely): use absolute path for migrationFolder in kysely.config.ts

--- apps/web/.config/kysely.config.ts
+++ apps/web/.config/kysely.config.ts
@@ -1,11 +1,15 @@
 import process from 'node:process'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 import { defineConfig } from 'kysely-ctl'
 import { useDatabase } from '../utils/db'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   kysely: useDatabase({ databaseUrl: process.env.NUXT_DATABASE_URL || '' }),
   migrations: {
-    migrationFolder: '../migrations',
+    migrationFolder: path.resolve(__dirname, '../migrations'),
     allowOrder: 'any',
   },
 })
